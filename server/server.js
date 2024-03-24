a3bb7f3df646328f79fdb38d350865c2cc0e7baa
Enes Yağmur <enesyagmur99@gmail.com>
2024-03-24 16:42:13 +0300

    server bağlantısı yapıldı, mongoose ve dotenv kuruldu

--- server/server.js
+++ server/server.js
@@ -1,7 +1,24 @@
 const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.listen(4000, () => {
-  console.log("4000. port dinleniyor");
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
 });
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(4000, () => {
+      console.log("Bağlantı başarılı");
+      console.log("4000. port dinleniyor");
+    });
+  })
+  .catch((err) => {
+    console.log(err + "bağlantı başarısız");
+  });
