8a8a648e2e2e4c9b2eaa91f177f9cf9ea2a8236e
Enes Yağmur <enesyagmur99@gmail.com>
2024-03-24 21:20:16 +0300

    controller lar oluşturulduktan sonra router güncellendi

--- server/routes/blogsRoute.js
+++ server/routes/blogsRoute.js
@@ -1,17 +1,23 @@
 //routing : belli url adreslerine kullanıcıdan gelen istekleri ilgili fonksiyonlara yönlendirmeye yarar
 const express = require("express");
-const { blogCreate } = require("../controllers/blogControllers");
+const {
+  blogCreate,
+  blogsGet,
+  blogGet,
+  blogDelete,
+  blogUpdate,
+} = require("../controllers/blogControllers");
 
 const router = express.Router();
 
-// router.get("/");
+router.get("/", blogsGet);
 
-// router.get("/:id");
+router.get("/:id", blogGet);
 
 router.post("/", blogCreate);
 
-// router.delete("/:id");
+router.delete("/:id", blogDelete);
 
-// router.patch("/:id");
+router.patch("/:id", blogUpdate);
 
 module.exports = router;
