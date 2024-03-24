462266299583feb2d0160b74080105bc72781d30
Enes Yağmur <enesyagmur99@gmail.com>
2024-03-25 02:44:06 +0300

    blog oluşturma işlemi yapıldı

--- client/src/pages/write/Write.jsx
+++ client/src/pages/write/Write.jsx
@@ -3,9 +3,25 @@ import "./write.scss";
 import { GoPlusCircle } from "react-icons/go";
 
 const Write = () => {
-  const [text, setText] = useState(
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. At deserunt saepe obcaecati quis temporibus reiciendis a dicta placeat, eum ipsum quas vitae dolorem voluptate harum commodi error ut molestiae, neque optio? Aliquam consequatur nam, ad harum, expedita quasi aut nobis quaerat voluptates, nihil perspiciatis velit consectetur ex? Pariatur ab voluptas mollitia magnam, cupiditate illum nihil obcaecati at asperiores. Non ratione a inventore fugit sunt reprehenderit repellendus porro consectetur ea animi laudantium numquam veritatis, accusantium ipsum facere sequi vitae reiciendis ad, obcaecati sint voluptate possimus atque iure. Minus necessitatibus, similique totam adipisci vero maxime culpa qui fugiat doloremque enim ut iure."
-  );
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+
+  const createBlog = async () => {
+    const blog = { title, text };
+
+    const response = await fetch("/api/notes", {
+      method: "POST",
+      body: JSON.stringify(blog),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      setTitle("");
+      setText("");
+      alert("Blog oluşturuldu");
+    }
+  };
+
   return (
     <div className="write">
       <label htmlFor="image">
@@ -17,7 +33,12 @@ const Write = () => {
       </label>
       <input type="file" id="image" style={{ display: "none" }} />
 
-      <input className="title" type="text" placeholder="Başlık" />
+      <input
+        className="title"
+        type="text"
+        placeholder="Başlık"
+        onChange={(e) => setTitle(e.target.value)}
+      />
 
       <textarea
         name=""
@@ -26,9 +47,10 @@ const Write = () => {
         rows="20"
         placeholder="Herhangi bir metin yazın..."
         value={text}
+        onChange={(e) => setText(e.target.value)}
       ></textarea>
 
-      <button>Yayınla</button>
+      <button onClick={createBlog}>Yayınla</button>
     </div>
   );
 };
