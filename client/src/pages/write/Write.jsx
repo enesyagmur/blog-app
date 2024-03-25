76a4f1ebadadc51047c3c76725949ce6da83a117
Enes Yağmur <enesyagmur99@gmail.com>
2024-03-25 03:16:20 +0300

    write sayfasına kategori alanı eklendi

--- client/src/pages/write/Write.jsx
+++ client/src/pages/write/Write.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 import "./write.scss";
 import { GoPlusCircle } from "react-icons/go";
+import { FiMusic } from "react-icons/fi";
+import { IoIosFitness } from "react-icons/io";
+import { FaCode } from "react-icons/fa6";
+import { IoAirplaneOutline } from "react-icons/io5";
+import { IoFastFoodOutline } from "react-icons/io5";
 
 const Write = () => {
   const [title, setTitle] = useState("");
   const [text, setText] = useState("");
+  const [category, setCategory] = useState("");
 
   const createBlog = async () => {
     const blog = { title, text };
@@ -24,6 +30,8 @@ const Write = () => {
 
   return (
     <div className="write">
+      <input type="file" id="image" style={{ display: "none" }} />
+
       <label htmlFor="image">
         <img
           src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
@@ -31,14 +39,36 @@ const Write = () => {
         />
         <GoPlusCircle className="icon" />
       </label>
-      <input type="file" id="image" style={{ display: "none" }} />
-
-      <input
-        className="title"
-        type="text"
-        placeholder="Başlık"
-        onChange={(e) => setTitle(e.target.value)}
-      />
+      <div className="center-blog">
+        <input
+          className="title"
+          type="text"
+          placeholder="Başlık"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <div className="categories">
+          <FaCode
+            className={category === "teknoloji" ? "this-category" : "icon"}
+            onClick={() => setCategory("teknoloji")}
+          />
+          <FiMusic
+            className={category === "müzik" ? "this-category" : "icon"}
+            onClick={() => setCategory("müzik")}
+          />
+          <IoIosFitness
+            className={category === "spor" ? "this-category" : "icon"}
+            onClick={() => setCategory("spor")}
+          />
+          <IoAirplaneOutline
+            className={category === "seyahat" ? "this-category" : "icon"}
+            onClick={() => setCategory("seyahat")}
+          />
+          <IoFastFoodOutline
+            className={category === "yemek" ? "this-category" : "icon"}
+            onClick={() => setCategory("yemek")}
+          />
+        </div>
+      </div>
 
       <textarea
         name=""
