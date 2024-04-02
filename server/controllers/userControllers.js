6934020c5e2fded72e13fee16372869d14eab687
Enes Yağmur <enesyagmur99@gmail.com>
2024-04-03 01:10:48 +0300

    user kayıt server tarafında gerekli işlemler yapıldı

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,3 +1,4 @@
+const userModel = require("../models/userModel");
 const user = require("../models/userModel");
 
 const loginUser = async (req, res) => {
@@ -5,7 +6,14 @@ const loginUser = async (req, res) => {
 };
 
 const signupUser = async (req, res) => {
-  res.json({ message: "Signup işlemi başarılı" });
+  const { name, email, password, image } = req.body; // istek body sinden gerekli bilgileri alıyoruz
+
+  try {
+    const user = await userModel.signup(name, email, password, image);
+    res.status(200).json({ name, email, image });
+  } catch (error) {
+    res.status(400).json({ hata: error.message });
+  }
 };
 
 module.exports = {
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,32 +1,46 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Sema = mongoose.Schema;
 
-const userSema = Sema({
+const userSema = new Sema({
   name: {
     type: String,
-    required: [true, "İsim zorunlu"],
+    required: true,
   },
   email: {
     type: String,
-    required: [true, "Mail zorunlu"],
-    unique: [true, "Mail zaten kayıtlı"],
+    required: true,
+    unique: true,
   },
   password: {
     type: String,
-    required: [true, "Şifre zorunlu"],
+    required: true,
   },
   image: {
     type: String,
-    required: [true, "Resim zorunlu"],
+    required: true,
   },
 });
 
-userSema.static.signup = async function (name, email, password, image) {
+//kayıt fonksiyonu
+userSema.statics.signup = async function (name, email, password, image) {
   const checkEmail = await this.findOne({ email }); // db de aynı email den var mı diye arıyoruz
   if (checkEmail) {
     throw Error("Email zaten kayıtlı");
   }
+
+  const salt = await bcrypt.genSalt(10); // 10 basamaklı bir şifre oluşturuyoruz
+  const hashPassword = await bcrypt.hash(password, salt); // random şifre ile parolamızı eşleştiriyoruz
+
+  const newUser = await this.create({
+    name,
+    email,
+    password: hashPassword,
+    image,
+  });
+
+  return newUser;
 };
 
 module.exports = mongoose.model("User", userSema);
