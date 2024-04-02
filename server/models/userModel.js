ae7c5f8c939be0e61acfab31cf96418434ae5ac7
Enes Yağmur <enesyagmur99@gmail.com>
2024-04-03 01:20:24 +0300

    signup fonksiyonu için validator ile email, password kontrolü

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Sema = mongoose.Schema;
 
@@ -25,13 +26,27 @@ const userSema = new Sema({
 
 //kayıt fonksiyonu
 userSema.statics.signup = async function (name, email, password, image) {
-  const checkEmail = await this.findOne({ email }); // db de aynı email den var mı diye arıyoruz
+  if (!email || !password || !name || !image) {
+    throw Error("Bilgiler boş bırakılamaz");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error("Email geçersiz");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error(
+      "Parola en az 8 karakter uzunluğunda olmalı, küçük, büyük harf, rakam, özel karakter içermelidir."
+    );
+  }
+
+  const checkEmail = await this.findOne({ email });
   if (checkEmail) {
     throw Error("Email zaten kayıtlı");
   }
 
-  const salt = await bcrypt.genSalt(10); // 10 basamaklı bir şifre oluşturuyoruz
-  const hashPassword = await bcrypt.hash(password, salt); // random şifre ile parolamızı eşleştiriyoruz
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
 
   const newUser = await this.create({
     name,
