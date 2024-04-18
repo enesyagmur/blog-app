2c014581fc5193b889a7e3e767ec0f1068d4dba4
Enes Yağmur <enesyagmur99@gmail.com>
2024-04-18 15:29:33 +0300

    kullanıcı günceleme işlemine giriş

--- client/src/pages/Account/UpdateUser.jsx
+++ client/src/pages/Account/UpdateUser.jsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./updateUser.scss";
 import { ImImages } from "react-icons/im";
 
 const UpdateUser = () => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user"))
+  );
+  const [name, setName] = useState(currentUser.name);
+  const [email, setEmail] = useState(currentUser.email);
+  const [image, setImage] = useState(currentUser.image);
+  const [password, setPassword] = useState("");
+  const [newUser, setNewUser] = useState({});
+
+  const updateUser = async () => {
+    if (password) {
+      setNewUser({
+        name: name,
+        email: email,
+        image: image,
+        password: password,
+      });
+    } else {
+      setNewUser({
+        name: name,
+        email: email,
+        image: image,
+      });
+    }
+    const response = await fetch("/api/user/account", {
+      method: "PATCH",
+      body: JSON.stringify(newUser),
+      headers: { "Content-Type": "application/json" },
+    });
+    const responseJson = await response.json();
+
+    if (response.ok) {
+      alert("kullanıcı güncellendi");
+    } else {
+      alert(responseJson.hata);
+    }
+  };
+
   return (
     <div className="updateUser">
       <p className="title">Profil Güncelleme</p>
@@ -11,13 +49,28 @@ const UpdateUser = () => {
         <ImImages className="icon" />
       </label>
       {/* <input type="file" id="image" style={{ display: "none" }} /> */}
-      <label htmlFor="user">Kullanıcı</label>
-      <input type="text" id="user" />
+      <label htmlFor="name">Kullanıcı</label>
+      <input
+        type="text"
+        id="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <label htmlFor="email">Email</label>
-      <input type="email" id="email" />
-      <label htmlFor="sifre">Şifre</label>
-      <input type="password" id="sifre" />
-      <button>Güncelle</button>
+      <input
+        type="email"
+        id="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <label htmlFor="password">Şifre</label>
+      <input
+        type="password"
+        id="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={updateUser}>Güncelle</button>
       <button className="delete">Hesabımı Sil</button>
     </div>
   );
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,15 @@
 const express = require("express");
 
-const { loginUser, signupUser } = require("../controllers/userControllers");
+const {
+  loginUser,
+  signupUser,
+  userUpdate,
+} = require("../controllers/userControllers");
 
 const router = express.Router();
 
 router.post("/login", loginUser);
 router.post("/signup", signupUser);
+router.post("/account", userUpdate);
 
 module.exports = router;
